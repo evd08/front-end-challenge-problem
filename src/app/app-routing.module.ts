26830a6c8342c5c110994c98b8685f10a16c4fee
eunice <eunice.dayrit09@gmail.com>
2020-09-24 12:32:11 -0700

    Added link to the home component route to equipments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {EquipmentsComponent} from './equipments/equipments.component';
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'equipments', component: EquipmentsComponent}
 ];
 
 @NgModule({
