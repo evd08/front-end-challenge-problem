030c7bd49b9e0b97344fc29e3ba814db0d85b0a4
eunice <eunice.dayrit09@gmail.com>
2020-10-01 22:27:17 -0700

    Added some styling on equipments index

--- src/app/equipments/equipments.component.ts
+++ src/app/equipments/equipments.component.ts
@@ -19,4 +19,8 @@ export class EquipmentsComponent implements OnInit {
     })
   }
 
+  isArray(val) {
+    return Array.isArray(val);
+  }
+
 }
--- src/app/services/equipment.service.ts
+++ src/app/services/equipment.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import  { Equipment } from '../models/Equipment';
+import { Equipment } from '../models/Equipment';
 
 @Injectable({
   providedIn: 'root'
